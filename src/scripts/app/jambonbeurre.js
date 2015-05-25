c272debf29c5506a47656ac700c88a1936217b2a
Fabien Maronnaud <fmaronnaud@klick.com>
2015-05-24 22:53:45 -0400

    feat(core javascript): first core library release
    - refactor previous code to work with requirejs
    - options(scroll): boolean to enable scrollbars in menu
    - options(scrollcontainer): css selector for the menu scrollable area (default: .jb-scroll-container)
    - options(menu): css selector for the menu (default: .jb-menu)
    - options(stickyheader): css selector for the sticky header (default: .jb-sticky-header)
    - options(triggers): array of css selector for the menu triggers (default: .jb-menu-trigger, .jb-shield)
    - options(content): css selector for the content area (default: .jb-content-wrapper)
    - options(keys): array of keyboard keys
    - iscroll is required to handle scrollbars in the menu
    - jquery is required for easier code writting. will have to be removed and writte in vanilla javascript in the future (but we are assuming that 99% of the website using javascript are using jquery)
    - when menu is opened, we disable native window scrolling (mainly because of ios since it is really handling badly fixed element). On desktop, it is possible to disable keyboard keys as well
    - when user is resizing the browser window and if the menu has a different aspect on desktop, the css transition will not be restarted
    -

---
+++ src/scripts/app/jambonbeurre.js
@@ -0,0 +1,134 @@
+define(['jquery', 'iscroll'], function ($) {
+    var jambonBeurre = function (options) {
+        this.opts = {
+            scroll: true,
+            scrollcontainer: ".jb-scroll-container",
+            menu: ".jb-menu",
+            stickyheader: ".jb-sticky-header",
+            triggers: [".jb-menu-trigger",".jb-shield"],            
+            content: ".jb-content-wrapper",
+            keys: [37, 38, 39, 40]             
+        };
+
+        this.init = function(){
+            var self = this, 
+                opts = $.extend({}, this.opts, options),
+                menu = $(opts.menu);
+            
+            $(document).ready(function(){
+                self.setMenu(opts);
+            });
+
+            $(opts.triggers.join()).on('click.jb-menu', function(e) {
+                e.preventDefault();
+                if($("body").attr('data-jb-state') == 'open') {
+                    self.hideMenu(opts);
+                } else {
+                    self.showMenu(opts);
+                }
+            });
+        }; 
+
+        this.setMenu = function(opts){
+            var menu = $(opts.menu);
+            $("body").attr('data-jb-state', 'closed');
+            if
+            (
+                typeof(IScroll) != 'undefined' //if IScroll 
+                && $(opts.menu + ">" + opts.scrollcontainer).length > 0
+                && opts.scroll
+            ){                
+                opts.menu_scroll = new IScroll(".jb-menu", { 
+                    click: true,
+                    scrollbars: true,
+                    scrollbarClass: 'jb-scrollbar',
+                    mouseWheel:true
+                });
+            }       
+        };
+
+        this.hideMenu = function(){
+            self = this;
+            $("body")
+                .attr("data-jb-state", "closed")
+                .addClass("is-nav-animate");
+
+            $(self.opts.stickyheader).css({ 
+                position: 'fixed',
+                top: 0
+            });
+
+            self.stopAnimation();
+            this.enable_scroll();            
+        };
+
+        this.showMenu = function(){
+            self = this;
+            $("body")
+                .attr("data-jb-state", "open")
+                .addClass("is-nav-animate");
+
+            $(self.opts.stickyheader).css({ 
+                position: 'absolute',
+                top: $('body').scrollTop()
+            });
+
+            self.stopAnimation();
+            this.disable_scroll(); 
+        };
+
+        this.preventDefault = function(e) {
+            e = e || window.event;
+            if (e.preventDefault) { e.preventDefault(); }
+            e.returnValue = false;
+        };
+
+        this.keydown = function(e) {
+            for (var i = self.opts.keys.length; i--;) {
+                if (e.keyCode === self.opts.keys[i]) {
+                    self.preventDefault(e);
+                    return;
+                }
+            }
+        };
+
+        this.wheel = function(e) { 
+            self.preventDefault(e);
+        };
+
+        this.disable_scroll = function() {
+            if (window.addEventListener) { 
+                window.addEventListener('DOMMouseScroll', self.wheel, false);
+            }
+
+            window.onmousewheel = document.onmousewheel = self.wheel;            
+            document.onkeydown = self.keydown;
+            document.ontouchmove = function (e) { 
+                self.preventDefault(e);
+            };
+        };
+
+        this.enable_scroll = function() {
+            if (window.removeEventListener) { 
+                window.removeEventListener('DOMMouseScroll', self.wheel, false); 
+            }
+
+            window.onmousewheel = document.onmousewheel = document.onkeydown = null;
+            document.ontouchmove = function (e) { 
+                return true;
+            };
+        };
+
+        this.stopAnimation =  function() {
+            setTimeout(function() { 
+                $('body').removeClass('is-nav-animate');
+            }, 751); 
+        };
+
+        this.init();
+    };
+
+    return jambonBeurre;
+ 
+});
+
