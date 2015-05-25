fa7c6556270b123a402ff5eb910574e527210820
Fabien Maronnaud <fmaronnaud@klick.com>
2015-05-24 23:22:49 -0400

    chore(gruntfile.js): fix issue with CHANGELOG commit url

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function(grunt) {
         version: '<%= bwr.version %>',
         repository: '<%= bwr.homepage %>',
         commitLink: function(commitHash){
-          return grunt.file.readJSON('bower.json').homepage + '/changesets/' + commitHash;
+          return grunt.file.readJSON('bower.json').homepage + '/commits/' + commitHash;
         },
         issueLink: function(issueId){
           return 'http://genome.klick.com/tickets/#/details/' + issueId;
