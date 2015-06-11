77f29dcb77edd8114fe8109c00553fe6581c6758
fabien <fmaronnaud@klick.com>
2015-06-10 23:21:48 -0400

    updates

--- scripts/common.js
+++ scripts/common.js
@@ -5,7 +5,7 @@ requirejs.config({
 		"iscroll": "https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.1/iscroll-min",
 		"hammerjs": "https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.4/hammer.min",
 		"bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
-		"jambonbeurre": "https://raw.githubusercontent.com/Jumpyjumper/JambonBeurre/master/dist/scripts/jambonbeurre.js"
+		"jambonbeurre": "dist/scripts/jambonbeurre"
 	}
 });
 
